95f0552c90ad4fb604909725103fdefe0e1a9754
Desktop Computer <wolfbladev3.0@gmail.com>
2018-02-18 01:48:38 -0800

    test commit

--- src/org/usfirst/frc/team1160/robot/TrajectoryWaypoints.java
+++ src/org/usfirst/frc/team1160/robot/TrajectoryWaypoints.java
@@ -4,6 +4,8 @@ import jaci.pathfinder.Waypoint;
 
 public interface TrajectoryWaypoints {
 	
+	//TODO: Slap a bunch of waypoint arrays here
+	
 	public static final Waypoint[] POINTS_1 =
 			new Waypoint[] {new Waypoint(0, 0, 0), new Waypoint(3, 0, 0),};
 
